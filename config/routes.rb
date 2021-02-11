c1b988fa8243b7020edb6af67d921f9415b0ffb0
Thomas Desmoulins <thomas.desmoulins@gmail.com>
2021-02-11 17:21:09 +0100

    model tag and plant_tag ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :gardens do
+  root to: 'gardens#index'
+
+  resources :gardens, only: [:index, :show] do
     resources :plants, only: [:create]
   end
 
